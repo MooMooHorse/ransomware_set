37c0c8a3a36b527cd5c6f65bd646c87dbe02b472
MooMooHorse <haor.20@itl.zju.edu.cn>
2023-05-16 03:26:55 +0000

    fix blktrace problem

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -44,6 +44,7 @@ def add_magic_num_1(tar_sys_path):
                 f.write(bytes([MAGIC_NUM["MAGIC_NUM1_02"]]))
                 f.write(bytes([MAGIC_NUM["MAGIC_NUM1_03"]]))
                 f.write(bytes([MAGIC_NUM["MAGIC_NUM1_04"]]))
+                f.flush()
 
 def add_magic_num_2(tar_sys_path):
     """
@@ -68,6 +69,7 @@ def add_magic_num_3(tar_sys_path):
             with open(os.path.join(root, file), "wb") as f:
                 for byte in data:
                     f.write(bytes([MAGIC_NUM["MAGIC_NUM3"]]))
+                f.flush()
 
 def launch_blktrace():
     default_disk = BOOT_CONFIG["default_disk"]
@@ -82,7 +84,10 @@ def launch_blktrace():
 
     def _launch_blktrace(device, output_file):
         command = ['sudo', 'blktrace', '-d', device, '-o', output_file]
-        return subprocess.Popen(command)
+        # get the current path 
+        path = os.getcwd()
+        process = subprocess.Popen(command, cwd = path)
+        return process
 
     blktrace_process_pid = _launch_blktrace(default_disk, default_trace_file_path).pid + 1
 
@@ -90,13 +95,51 @@ def launch_blktrace():
     with open(f"{blktrace_dir}/blktrace_pid", "w+") as f:
         f.write(f"{blktrace_process_pid}")
 
+def dump_trace_file():
+    # first we shut down blktrace
+    os.system("sudo pkill blktrace")
+    default_trace_file_path = BOOT_CONFIG["default_trace_file_path"]
+    blktrace_dir = BOOT_CONFIG["blktrace_dir"]
+    import subprocess
+    def _launch_blkparse(trace_file, output_file):
+        # sudo blkparse -i tracefile -f "%T %S %3d\n"
+        # os.system('pwd')
+        command = ['sudo', 'blkparse', '-i', trace_file, '-f', '%T %S %3d\n', '-o', output_file]
+        path = os.getcwd()
+        process = subprocess.run(command, cwd = path)
+    # sleep for 0.2 seconds to make sure blktrace is shut down
+    import time
+    time.sleep(0.2)
+    _launch_blkparse(default_trace_file_path, f"{blktrace_dir}/blkparse_output")
+    # now we can remove the trace files
+    # os.system(f"sudo rm {default_trace_file_path}*")
+    # now we re-launch blktrace
+    launch_blktrace()
+    
+
+def files_sync(tar_sys_path):
+    """
+    Sync all the files in target system to disk
+    Refer to https://stackoverflow.com/questions/15983272/does-python-have-sync
+    """
+    if hasattr(os, 'sync'):
+        sync = os.sync
+    else:
+        import ctypes
+        libc = ctypes.CDLL("libc.so.6")
+        def sync():
+            libc.sync()
+    sync()
+
 def preprocess_tar_sys(tar_sys_path):
     """
     Add magic number to the target system
     """
+    files_sync(tar_sys_path)
     launch_blktrace()
     add_magic_num_1(tar_sys_path)
     add_magic_num_2(tar_sys_path)
     add_magic_num_3(tar_sys_path)
-
+    files_sync(tar_sys_path)
+    dump_trace_file()
     
