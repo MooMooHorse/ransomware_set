a1d894954ce2aea60502375dd4e597676ca2caae
MooMooHorse <haor.20@itl.zju.edu.cn>
2023-05-19 21:17:44 +0000

    remove redundant kernel code

--- linux/modinstall.sh
+++
@@ -1,1 +0,0 @@
-make -j60 modules_install && make install
--- linux/ssd/backend.h
+++
@@ -1,8 +0,0 @@
-// #ifndef __BACKEND_H__
-// #define __BACKEND_H__
-
-// #include "ftl.h"
-// // #include <linux/circ_buf.h>
-// // #include <linux/kfifo.h>
-
-// #endif
