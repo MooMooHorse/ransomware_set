8009738eedaa586abfd7f8c8f3f4bfd15b34bb86
Hao Ren <haor.20@intl.zju.edu.cn>
2023-06-12 17:01:05 +0000

    geneate tar_sys finished

--- impressions/utils/gen.py
+++ impressions/utils/gen.py
@@ -3,6 +3,49 @@ import shutil
 import sys
 
 
+
+# default config for the target system
+
+bacth_index = 1
+path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+stat_dir = os.path.join(path, 'stat')
+config_dir = os.path.join(path, 'config')
+
+
+parent_path = "/home/h/haha 1"
+actual_logfile = f"{path}/logs" # log directory
+if not os.path.exists(actual_logfile):
+    os.mkdir(actual_logfile)
+actual_logfile = actual_logfile + " 1"
+    
+randseeds = [10, 100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 100000]
+fs_capacity = "200 GB"
+fs_used = "10 GB"
+num_files = "NO K"
+num_dirs = "NO K"
+files_per_dir = "10.2 N"
+file_size_distr = "Direct 3 99994 29 0.91"
+file_count_distr = "Direct 2 9.48 2.45663283"
+dir_count_files = "Direct 2 2 2.36"
+dir_size_subdir_distr = "Indir DirsizesubdirDistr.txt"
+files_with_depth = "Direct 10"
+layout_score = "1.0"
+actual_file_creation = "1"
+special_flags = "10"
+flat = 0
+deep = 0
+ext = -1
+wordfreq = 0
+large2small = 0
+small2large = 0
+depthwithcare = 1
+filedepthpoisson = 1
+dircountfiles = 0
+constraint = 0
+printwhat = 10
+
+
+
 def clean_sys(inputfile_path = '../inputfile.sample'):
     with open(inputfile_path, 'r') as f:
         for line in f:
@@ -19,6 +62,14 @@ def clean_sys(inputfile_path = '../inputfile.sample'):
                     shutil.rmtree(path)
 
 def handle_flags():
+    """
+    Handle the flags passed to the script.
+    An example would be :
+    python3 utils/gen.py --clean
+    Another example would be :
+    rm -rf /home/h/rans_test && mkdir /home/h/rans_test && mkdir /home/h/rans_test/tar_sys && python3 /home/h/ransomware_set/impressions/utils/gen.py -path=/home/h/rans_test/tar_sys -batch=2 -tused=10 -usedunit=GB -mu=2 -fscore=1.0
+    """
+    global parent_path, bacth_index, fs_used, file_count_distr, layout_score
     for arg in sys.argv[1:]:
         if arg.startswith("--clean"):
             config_path = arg.split('=')
@@ -34,15 +85,17 @@ def handle_flags():
                 os.remove(path + '/' + config_path)
             sys.exit(0)
         elif arg.startswith("-path"):
-            parent_path = arg.split('=')[1].strip()
+            parent_path = arg.split('=')[1].strip() + ' 1'
         elif arg.startswith('-batch'):
-            bacth_index = arg.split('=')[1].strip()
-        elif arg.startswith('-used'):
+            bacth_index = int(arg.split('=')[1].strip())
+        elif arg.startswith('-tused'):
             fs_used = arg.split('=')[1].strip()
+        elif arg.startswith('-usedunit'):
+            fs_used = fs_used + ' ' + arg.split('=')[1].strip()
         elif arg.startswith('-mu'):
             file_count_distr = "Direct 2 " + arg.split('=')[1].strip() + " 2.45663283"
         elif arg.startswith('-fscore'):
-            layout_score = arg.split('=')[1].strip()
+            layout_score = float(arg.split('=')[1].strip())
             
 def generate_config_file(parent_path, actual_logfile, randseeds, fs_capacity, fs_used, num_files, num_dirs, \
     files_per_dir, file_size_distr, file_count_distr, dir_count_files, dir_size_subdir_distr, \
@@ -89,64 +142,35 @@ def generate_config_file(parent_path, actual_logfile, randseeds, fs_capacity, fs
 
     return config
 
-bacth_index = 1
-path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-stat_dir = os.path.join(path, 'stat')
-config_dir = os.path.join(path, 'config')
-if not os.path.exists(config_dir):
-    os.mkdir(config_dir)
-config_path = config_dir + f'/config_{bacth_index}'
-
-parent_path = "/home/h/haha 1"
-actual_logfile = f"{path}/logs" # log directory
-if not os.path.exists(actual_logfile):
-    os.mkdir(actual_logfile)
-actual_logfile = actual_logfile + " 1"
+def main():
+    global parent_path, bacth_index, fs_used, file_count_distr, layout_score
     
-randseeds = [10, 100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 100000]
-fs_capacity = "200 GB"
-fs_used = "10 GB"
-num_files = "NO K"
-num_dirs = "NO K"
-files_per_dir = "10.2 N"
-file_size_distr = "Direct 3 99994 29 0.91"
-file_count_distr = "Direct 2 9.48 2.45663283"
-dir_count_files = "Direct 2 2 2.36"
-dir_size_subdir_distr = "Indir DirsizesubdirDistr.txt"
-files_with_depth = "Direct 10"
-layout_score = "1.0"
-actual_file_creation = "1"
-special_flags = "10"
-flat = 0
-deep = 0
-ext = -1
-wordfreq = 0
-large2small = 0
-small2large = 0
-depthwithcare = 1
-filedepthpoisson = 1
-dircountfiles = 0
-constraint = 0
-printwhat = 10
-
-handle_flags()
-
-config_file = generate_config_file(parent_path, actual_logfile, randseeds, fs_capacity, fs_used, num_files, num_dirs, files_per_dir, \
-    file_size_distr, file_count_distr, dir_count_files, dir_size_subdir_distr, files_with_depth, layout_score, actual_file_creation, \
-        special_flags, flat, deep, ext, wordfreq, large2small, small2large, depthwithcare, filedepthpoisson, dircountfiles, constraint, printwhat)
-
-
-
-# dump the string to config file
-with open(config_path, 'w') as f:
-    f.write(config_file)
-
-impression_path = path + '/impressions'
-parent_path = parent_path.split(' ')[0].strip()
-# stat dir 
-if not os.path.exists(stat_dir):
-    os.mkdir(stat_dir)
+    handle_flags()
     
-cmd = f"rm -rf {parent_path} && mkdir {parent_path} && {impression_path} {config_path} {bacth_index} > {stat_dir}/stat_{bacth_index}"
+    if not os.path.exists(config_dir):
+        os.mkdir(config_dir)
+    config_path = config_dir + f'/config_{bacth_index}'
 
-os.system(cmd)
+    config_file = generate_config_file(parent_path, actual_logfile, randseeds, fs_capacity, fs_used, num_files, num_dirs, files_per_dir, \
+        file_size_distr, file_count_distr, dir_count_files, dir_size_subdir_distr, files_with_depth, layout_score, actual_file_creation, \
+            special_flags, flat, deep, ext, wordfreq, large2small, small2large, depthwithcare, filedepthpoisson, dircountfiles, constraint, printwhat)
+
+
+    # dump the string to config file
+    with open(config_path, 'w') as f:
+        f.write(config_file)
+    # print(config_file)
+    # print(config_path)
+    
+    impression_path = path + '/impressions'
+    parent_path = parent_path.split(' ')[0].strip()
+    # stat dir 
+    if not os.path.exists(stat_dir):
+        os.mkdir(stat_dir)
+        
+    cmd = f"rm -rf {parent_path} && mkdir {parent_path} && {impression_path} {config_path} {bacth_index} > {stat_dir}/stat_{bacth_index}"
+
+    os.system(cmd)
+    
+if __name__ == '__main__':
+    main()
