ec6c7b427be2892a1eccfd077f0f753d8a4c0d9b
Hao Ren <haor.20@intl.zju.edu.cn>
2023-07-16 19:41:07 +0000

    add dump blocks

--- utils/syscall/blk2file.cpp
+++ utils/syscall/blk2file.cpp
@@ -9,6 +9,12 @@
 #define MAX_FILES 40000
 #define MAX_FNAME_LEN 32
 
+void general_dump_blocks(const uint64_t* blocks, const int num_blocks) {
+    for(int i = 0; i < num_blocks; i++) {
+        printf("%lu ", blocks[i]);
+    }
+}
+
 int main() {
     uint64_t blocks[MAX_BLCOKS];
     char** filenames;
@@ -20,5 +26,6 @@ int main() {
     int num_files = get_filenames(filenames, MAX_FILES);
     std::cout << "num_blocks: " << num_blocks << " " << "num_files: " << num_files << std::endl;
     dump_blocks_f2fs(blocks, num_blocks, (const char**)filenames, num_files);
+    // general_dump_blocks(blocks, num_blocks);
     return 0;
 }
